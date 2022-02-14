94d28d0feb97b90bb713109f31c0501150a4c430
BaturinaYu <baturina_yu@mail.ru>
2022-02-14 16:29:54 +1200

    Скрипт к заданию 1, урок 1

---
+++ python_class2/less1_z1.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+username = 'BaturinaYu'
+url = f'https://api.github.com/users/{username}/repos'
+
+get_from_git = requests.get(url)
+repos_dict = get_from_git.json()
+print(f'Репозитории пользователя {username} на github.com:')
+for i, el in enumerate(repos_dict):
+    print(i+1, el['name'])
+
+with open('data.txt', 'w') as outfile:
+    json.dump(repos_dict, outfile, indent=4)
